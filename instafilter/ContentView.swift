243cc0d97bd64286977a3f7f4dfadba09f559b2f
travisbrigman <travis1000@gmail.com>
2021-02-24 07:55:01 -0600

    Initial Commit

---
+++ instafilter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  instafilter
+//
+//  Created by Travis Brigman on 2/24/21.
+//  Copyright © 2021 Travis Brigman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
